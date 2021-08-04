173d8886d2ddf8d4137b711c8baac4103b1c5f05
Vidas P <vp@automaticmode.com>
2021-08-04 21:47:49 +0300

    Cache worker status for multiple browsers(tabs)

--- app/controllers/worker_status_controller.rb
+++ app/controllers/worker_status_controller.rb
@@ -4,22 +4,27 @@ class WorkerStatusController < ApplicationController
 
     since_id = params[:since_id].presence || 0
 
-    messages = current_user.messages.where('id > ?', since_id)
+    cache_key = "worker_status/since_id-#{since_id}-user_id-#{current_user.id}"
 
-    result = messages.select('COUNT(id) AS count', 'MAX(id) AS max_id')
-      .reorder(Arel.sql('min(created_at)')).first
-    count, max_id = result.count, result.max_id
+    worker_status = Rails.cache.fetch(cache_key, expires_in: 2.seconds) do
+      messages = current_user.messages.where('id > ?', since_id)
 
-    # Max aggregate only works if there are any new messages.
-    max_id = since_id if count == 0
+      result = messages.select('COUNT(id) AS count', 'MAX(id) AS max_id')
+        .reorder(Arel.sql('min(created_at)')).first
+      count, max_id = result.count, result.max_id
 
-    render json: {
-      pending: Delayed::Job.pending.where('run_at <= ?', start).count,
-      awaiting_retry: Delayed::Job.awaiting_retry.count,
-      recent_failures: Delayed::Job.failed_jobs.where('failed_at > ?', 5.days.ago).count,
-      message_count: count,
-      max_id: max_id || 0,
-      compute_time: Time.now - start
-    }
+      # Max aggregate only works if there are any new messages.
+      max_id = since_id if count == 0
+
+      {
+        pending: Delayed::Job.pending.where('run_at <= ?', start).count,
+        awaiting_retry: Delayed::Job.awaiting_retry.count,
+        recent_failures: Delayed::Job.failed_jobs.where('failed_at > ?', 5.days.ago).count,
+        message_count: count,
+        max_id: max_id || 0
+      }
+    end
+
+    render json: worker_status
   end
 end
