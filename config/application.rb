018160fa4d14bec3ef68be1ee4c6f7bd83a68033
Vidas P <vp@automaticmode.com>
2023-04-03 09:21:58 +0300

    Only use required rails ties

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,20 @@
 require_relative 'boot'
 
-require 'rails/all'
+#require 'rails/all'
+require 'rails'
+
+require 'active_model/railtie'
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 require 'dotenv'
 
